c18c12cb6962624625c23e522b5844b0c749dc85
Natalia Khizrieva <jesusstern@gmail.com>
2023-09-14 20:17:06 +0300

    Lab 2 "Overriding" added

--- Demo/VPExamples/ExampleUI/Program.cs
+++ Demo/VPExamples/ExampleUI/Program.cs
@@ -15,7 +15,7 @@ namespace ExampleUI
     {
         static void Main(string[] args)
         {
-            //Inheritance();
+            Inheritance();
             //StringIntern();
             //Delegates();
             //Exceptions();
@@ -29,13 +29,13 @@ namespace ExampleUI
             //var fs2 = new FileStream(@"D:/User/Source/NewDir/file.txt", FileMode.OpenOrCreate);
             //fs2.Read(arr);
 
-            Console.WriteLine(GC.GetTotalMemory(false));
-            var c1 = "123";
-            var c2 = "457";
-            Console.WriteLine(GC.GetTotalMemory(false));
-            GC.Collect();
+            // Console.WriteLine(GC.GetTotalMemory(false));
+            // var c1 = "123";
+            // var c2 = "457";
+            // Console.WriteLine(GC.GetTotalMemory(false));
+            // GC.Collect();
 
-            Console.WriteLine(GC.GetTotalMemory(false));
+            // Console.WriteLine(GC.GetTotalMemory(false));
         }
 
         static void Inheritance()
@@ -50,79 +50,79 @@ namespace ExampleUI
             }
         }
 
-        static void StringIntern()
-        {
-            string s1 = "str";
-            string s2 = new StringBuilder().Append("s").Append("tr").ToString();
-            string s3 = String.Intern(s2);
-            string s4 = "str";
-            //s4 = s1;
-            s3 = String.Intern("123");
+        // static void StringIntern()
+        // {
+        //     string s1 = "str";
+        //     string s2 = new StringBuilder().Append("s").Append("tr").ToString();
+        //     string s3 = String.Intern(s2);
+        //     string s4 = "str";
+        //     //s4 = s1;
+        //     s3 = String.Intern("123");
 
-            Console.WriteLine($"s1 = {s1}");
-            Console.WriteLine($"s2 = {s2}");
-            Console.WriteLine($"s3 = {s3}");
-            Console.WriteLine($"s4 = {s4}");
+        //     Console.WriteLine($"s1 = {s1}");
+        //     Console.WriteLine($"s2 = {s2}");
+        //     Console.WriteLine($"s3 = {s3}");
+        //     Console.WriteLine($"s4 = {s4}");
 
-            Console.WriteLine($"s2 has the same reference as s1: {(Object)s2 == (Object)s1}");
-            Console.WriteLine($"s3 has the same reference as s1: {(Object)s3 == (Object)s1}");
-            Console.WriteLine($"s3 has the same reference as s2: {(Object)s3 == (Object)s2}");
-            Console.WriteLine($"s4 has the same reference as s1: {(Object)s4 == (Object)s1}");
-            Console.WriteLine($"s4 has the same reference as s3: {(Object)s4 == (Object)s3}");
-        }
+        //     Console.WriteLine($"s2 has the same reference as s1: {(Object)s2 == (Object)s1}");
+        //     Console.WriteLine($"s3 has the same reference as s1: {(Object)s3 == (Object)s1}");
+        //     Console.WriteLine($"s3 has the same reference as s2: {(Object)s3 == (Object)s2}");
+        //     Console.WriteLine($"s4 has the same reference as s1: {(Object)s4 == (Object)s1}");
+        //     Console.WriteLine($"s4 has the same reference as s3: {(Object)s4 == (Object)s3}");
+        // }
 
-        static void Delegates()
-        {
-            DelegateExample.Example(10, 5);
+        // static void Delegates()
+        // {
+        //     DelegateExample.Example(10, 5);
 
-            //MulticastDelegateExample.Example(10, 5);
+        //     //MulticastDelegateExample.Example(10, 5);
 
-            //ActionDelegateExample.Example(10, 5);
+        //     //ActionDelegateExample.Example(10, 5);
 
-            //FuncDelegateExample.Example(10, 5);
-        }
+        //     //FuncDelegateExample.Example(10, 5);
+        // }
 
-        static void Exceptions()
-        {
-            //Example.Exception();
-            //Example.Demo();
-            Example.SpecialDemo();
-        }
+        // static void Exceptions()
+        // {
+        //     //Example.Exception();
+        //     //Example.Demo();
+        //     Example.SpecialDemo();
+        // }
 
-        static void Arguments()
-        {
-            ArgumentsExample.ArgumentsExample.Demo();
-        }
+        // static void Arguments()
+        // {
+        //     ArgumentsExample.ArgumentsExample.Demo();
+        // }
 
-        static void Events()
-        {
-            //EventDemo.FirstDemo();
-            //EventDemo.SecondDemo();
-            //EventDemo.ThirdDemo();
-            //EventDemo.FourthDemo();
+        // static void Events()
+        // {
+        //     //EventDemo.FirstDemo();
+        //     //EventDemo.SecondDemo();
+        //     //EventDemo.ThirdDemo();
+        //     //EventDemo.FourthDemo();
 
-            var dianovDocs = new StudentDocs("Дианов", 140);
-            dianovDocs.AddTest("Физкультура", 99);
-            dianovDocs.AddTest("СиАКОД", 10);
-            //Попробуем вызвать метод у самого объекта напрямую
-            //Для этого сделали его public
-            dianovDocs._student.Test("Базы данных", 100);
-        }
+        //     var dianovDocs = new StudentDocs("Дианов", 140);
+        //     dianovDocs.AddTest("Физкультура", 99);
+        //     dianovDocs.AddTest("СиАКОД", 10);
+        //     //Попробуем вызвать метод у самого объекта напрямую
+        //     //Для этого сделали его public
+        //     dianovDocs._student.Test("Базы данных", 100);
+        // }
 
-        static void FileSystem()
-        {
-            //FSExample.DrivesDemo();
-            //FSExample.EnvironmentDemo();
-            //FSExample.DirectoryDemo();
-            //FSExample.DirectoryInfoDemo();
-            //FSExample.FileDemo();
-            //FSExample.FileInfoDemo();
-            //FSExample.FileStreamDemo();
-            //FSExample.StreamReaderDemo();
-            //FSExample.StreamWriterDemo();
-            //FSExample.BinaryWriterDemo();
-            //FSExample.BinaryReaderDemo();
-        }
+        // static void FileSystem()
+        // {
+        //     //FSExample.DrivesDemo();
+        //     //FSExample.EnvironmentDemo();
+        //     //FSExample.DirectoryDemo();
+        //     //FSExample.DirectoryInfoDemo();
+        //     //FSExample.FileDemo();
+        //     //FSExample.FileInfoDemo();
+        //     //FSExample.FileStreamDemo();
+        //     //FSExample.StreamReaderDemo();
+        //     //FSExample.StreamWriterDemo();
+        //     //FSExample.BinaryWriterDemo();
+        //     //FSExample.BinaryReaderDemo();
+        // }
 
     }
 }
